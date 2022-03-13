c36172853894eb63cbfce04a51bdffa7a001f9f5
ruljaka <ruljaka@gmail.com>
2022-03-13 16:06:12 +0300

    refactor CustomJsonConverter

--- app/src/main/java/com/ruslangrigoriev/exchangerates/data/api/CustomJsonConverter.kt
+++ app/src/main/java/com/ruslangrigoriev/exchangerates/data/api/CustomJsonConverter.kt
@@ -1,9 +1,7 @@
 package com.ruslangrigoriev.exchangerates.data.api
 
-import com.google.gson.JsonDeserializationContext
-import com.google.gson.JsonDeserializer
-import com.google.gson.JsonElement
-import com.google.gson.JsonObject
+import com.google.gson.*
+import com.google.gson.reflect.TypeToken
 import com.ruslangrigoriev.exchangerates.domain.model.Currency
 import com.ruslangrigoriev.exchangerates.domain.model.CurrencyRatesInfo
 import com.ruslangrigoriev.exchangerates.utils.extGetCurrentDate
@@ -16,65 +14,59 @@ class CustomJsonConverter : JsonDeserializer<CurrencyRatesInfo> {
         typeOfT: Type?,
         context: JsonDeserializationContext?
     ): CurrencyRatesInfo? {
-
-        val jsonObject = json?.asJsonObject;
-        jsonObject?.let {
+        return json?.asJsonObject?.let {
             val date = it.get("Date").asString
             val valute = it.get("Valute").asJsonObject
             val currencyList = mutableListOf<Currency>()
             val currentDate = extGetCurrentDate()
-            //такой вот неудобный Json прилетает с сервера
-            currencyList.add(valuteToCurrency(valute.get("AUD").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("AZN").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("GBP").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("AMD").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("BYN").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("BGN").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("BRL").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("HUF").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("HKD").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("DKK").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("USD").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("EUR").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("INR").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("KZT").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("CAD").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("KGS").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("CNY").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("MDL").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("NOK").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("PLN").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("RON").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("XDR").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("SGD").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("TJS").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("TRY").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("TMT").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("UZS").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("UAH").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("CZK").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("SEK").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("CHF").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("ZAR").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("KRW").asJsonObject))
-            currencyList.add(valuteToCurrency(valute.get("JPY").asJsonObject))
-            return CurrencyRatesInfo(
+            //добавление всех полей "Valute" в список
+            currencyList.add(jsonToCurrency(valute.get("AUD").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("AZN").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("GBP").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("AMD").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("BYN").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("BGN").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("BRL").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("HUF").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("HKD").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("DKK").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("USD").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("EUR").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("INR").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("KZT").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("CAD").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("KGS").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("CNY").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("MDL").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("NOK").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("PLN").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("RON").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("XDR").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("SGD").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("TJS").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("TRY").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("TMT").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("UZS").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("UAH").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("CZK").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("SEK").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("CHF").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("ZAR").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("KRW").asJsonObject))
+            currencyList.add(jsonToCurrency(valute.get("JPY").asJsonObject))
+            CurrencyRatesInfo(
                 id = 0,
                 date = date,
                 updated = currentDate,
                 currency = currencyList
             )
         }
-        return null
     }
 
-    private fun valuteToCurrency(jsonObject: JsonObject): Currency {
-        val id = jsonObject.get("ID").asString
-        val charCode = jsonObject.get("CharCode").asString
-        val name = jsonObject.get("Name").asString
-        val nominal = jsonObject.get("Nominal").asInt
-        val value = jsonObject.get("Value").asDouble
-        return Currency(id, charCode, name, nominal, value)
+    private fun jsonToCurrency(jsonObject: JsonObject): Currency {
+        val type = object : TypeToken<Currency>() {}.type
+        val gson = Gson()
+        return gson.fromJson(jsonObject, type)
     }
 
 }
